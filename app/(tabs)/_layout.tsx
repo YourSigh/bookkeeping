e84785032b90da38bccd596cd45a2943b98d1f63
绿桶 <1325285734@qq.com>
2024-10-16 23:25:29 +0800

    feat:路由&顶部样式

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,19 +1,45 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-
+import { View, Text, TouchableOpacity } from 'react-native';
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import { router, useSegments } from 'expo-router';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const segments = useSegments();
+
+  // 定义三个菜单的路由名称
+  const mainRoutes = ['(tabs)', 'tool', 'settings'];
+
+  // 自定义头部
+  const Header = () => {
+    return (
+      <View style={{ height: 60, justifyContent: 'center', alignItems: 'center', backgroundColor: '#ccffaa' }}>
+        {!mainRoutes.includes(segments[segments.length - 1]) && (
+          <TouchableOpacity  style={{ position: 'absolute', left: 10 }} onPress={() => router.back()}>
+            <AntDesign name="arrowleft" size={24} color={Colors[colorScheme ?? 'light'].text} />
+          </TouchableOpacity>
+        )}
+        <Text style={{ fontSize: 20, color: Colors[colorScheme ?? 'light'].text }}>
+          记账APP
+        </Text>
+      </View>
+    );
+  };
+  
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
+        tabBarActiveTintColor: '#1daa1d',
+        tabBarInactiveTintColor: Colors[colorScheme?? 'light'].text,
+        tabBarStyle: {
+          backgroundColor: '#ccffaa', // 设置背景颜色
+        },
+        header: () => <Header />,
       }}>
       <Tabs.Screen
         name="index"
@@ -21,6 +47,15 @@ export default function TabLayout() {
           title: '首页',
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="tool"
+        options={{
+          title: '工具',
+          tabBarIcon: ({ color, focused }) => (
+            <AntDesign name="tool" size={28} color={color} />
           ),
         }}
       />
