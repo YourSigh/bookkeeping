7b0c6b891943687418dc9286dbf5fcb11bc6f914
绿桶 <1325285734@qq.com>
2024-11-14 20:29:37 +0800

    feat:扫雷难度数据

---
+++ app/pages/tool/components/landmine/model/landmine.ts
@@ -0,0 +1,18 @@
+// 不同难度的地图大小，雷等信息
+export const landmineMap = [
+  {
+    column: 9,
+    row: 9,
+    mine: 10,
+  },
+  {
+    column: 16,
+    row: 16,
+    mine: 40,
+  },
+  {
+    column: 30,
+    row: 16,
+    mine: 99,
+  },
+];
